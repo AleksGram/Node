705aa39423374cb4190f8a7e7e2b4e533298a4ff
alexandergr <alexanderg@hexagontech.co.il>
2020-02-24 20:47:27 +0200

    added recursive reader

--- logger.js
+++ logger.js
@@ -3,33 +3,45 @@ const { extname } = require("path");
 require('dotenv').config()
 
 const { createExtensionsMap } = require("./utils.js");
-const { startLog } = require('./parser.js');
+const { startLog, parseFiles } = require('./parser.js');
 
 
 
 exports.logger = function () {
     const argv = require('minimist')(process.argv.slice(2));
     const colors = argv._;
-    const deepLevelOfSearch = argv.deep ? argv.deep : null;
+    const deepLevelOfSearch = argv.deep ? argv.deep : undefined;
     const extensions = process.env.EXT ? process.env.EXT.split(',') : null;
-
-
-    const filesMap = createExtensionsMap(extensions, colors);
 
     const others = [];
 
-    const handleMatchFile = function (files) {
-        files.map(file => {
+    const handleMatchFile = function (error, files) {
+        if (error) throw error;
+
+        const colorsLenght = colors.length;
+        let currentColor = 0;
+        files.map((file, index) => {
             const fileExt = extname(file);
-            if (filesMap[fileExt]) {
-                logFile(filesMap[fileExt], file)
+
+            if (extensions && extensions.indexOf(fileExt.slice(1)) !== -1) {
+                logFile(colors[currentColor], file);
+                if ((currentColor += 1) < colorsLenght) {
+                    currentColor = currentColor;
+                } else currentColor = 0;
             } else {
                 if (others.indexOf(fileExt) === -1) {
                     others.push(fileExt);
                 }
             }
         })
+        if (others.length) {
+            console.log(`**********  Not muched extensions *********`);
+            others.map(ext => console.log(ext));
+        }
     }
+
+
+    /*  Uncomment for using filehound library
 
     startLog(deepLevelOfSearch, process.env.FILE_PATH || __dirname)
         .find((err, files) => {
@@ -39,6 +51,9 @@ exports.logger = function () {
                 others.map(ext => console.log(ext));
             }
         });
+    */
+
+    parseFiles(process.env.FILE_PATH || __dirname, handleMatchFile, deepLevelOfSearch);
 
 }
 
